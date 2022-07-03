ec2e4048e86eac4ff3e0e48d2adb8645a979f8f5
Kevin Bevan <iambevan@gmail.com>
2022-07-03 03:46:20 +0100

    grid index util

---
+++ src/utils/guessIndex.ts
@@ -0,0 +1,18 @@
+export const guessIndex = (index: number) => {
+    switch (index) {
+        case 0:
+            return "guess1";
+        case 1:
+            return "guess2";
+        case 2:
+            return "guess3";
+        case 3:
+            return "guess4";
+        case 4:
+            return "guess5";
+        case 5:
+            return "guess6";
+        default:
+            break;
+    }
+};
