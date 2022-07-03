bac948556ca72391651507428f67015972159d63
Kevin Bevan <iambevan@gmail.com>
2022-07-03 04:03:37 +0100

    fontstyle

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -4,7 +4,7 @@ interface CellProps {
 
 export function Cell({ letter }: CellProps) {
     return (
-        <div className="h-16 w-16 m-1 border-2 border-neutral-700 text-white">
+        <div className="h-16 w-16 m-1 border-2 border-neutral-700 text-white flex justify-center items-center text-grid">
             {letter}
         </div>
     );
