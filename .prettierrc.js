f5b29cca22e40c3f7d837c69b3870b6c68e4a933
Kevin Bevan <iambevan@gmail.com>
2022-07-17 01:54:02 +0100

    remove tracked folder

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-    tabWidth: 4,
-    useTabs: false,
-    printWidth: 80,
-    semi: true,
-    singleQuote: false,
-    jsxSingleQuote: true,
-    bracketSpacing: true,
-    arrowParens: "avoid",
+	tabWidth: 4,
+	useTabs: false,
+	printWidth: 80,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	arrowParens: "avoid",
+	endOfLine: "lf"
 };
