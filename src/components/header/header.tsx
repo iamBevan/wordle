987f16c06936f0ec093966cd2a0886f4e413f38a
Kevin Bevan <iambevan@gmail.com>
2022-07-18 00:28:25 +0100

    add some icons and buttons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,23 @@
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { BiBarChartAlt2 } from "react-icons/bi";
+import { FaCog } from "react-icons/fa";
 import styles from "./header.module.scss";
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <div>left</div>
+            <button>
+                <AiOutlineQuestionCircle />
+            </button>
             <div className={styles.title}>Wordle</div>
-            <div>right</div>
+            <div className={styles.actions}>
+                <button>
+                    <BiBarChartAlt2 />
+                </button>
+                <button>
+                    <FaCog />
+                </button>
+            </div>
         </header>
     );
 }
