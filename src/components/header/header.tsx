b32e1ef71516e8f242fae7161661201562ba6e5c
Kevin Bevan <iambevan@gmail.com>
2022-07-20 23:44:20 +0100

    add stats modal

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 import { BiBarChartAlt2 } from "react-icons/bi";
 import { FaCog } from "react-icons/fa";
 import styles from "./header.module.scss";
+import { StatsModal } from "./stats-modal";
 
 export function Header() {
+    const [statsOpen, setStatsOpen] = useState(false);
+
+    const onStatsClick = () => {
+        setStatsOpen(true);
+    };
+
     return (
         <header className={styles.header}>
             <button>
@@ -11,9 +19,14 @@ export function Header() {
             </button>
             <div className={styles.title}>Wordle</div>
             <div className={styles.actions}>
-                <button>
-                    <BiBarChartAlt2 />
-                </button>
+                <StatsModal
+                    open={statsOpen}
+                    trigger={
+                        <button onClick={onStatsClick}>
+                            <BiBarChartAlt2 />
+                        </button>
+                    }
+                />
                 <button>
                     <FaCog />
                 </button>
